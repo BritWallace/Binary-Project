044e6236cdb1264d3c6b3ed91a31457f31e21981
Brit Wallace <britwallace888@gmail.com>
2021-11-17 19:27:06 -0800

    First commit
    Co-authored-by: Wajma Niazi <w1nizi@hotmail.com>
    Co-authored-by: Nickolas Sullivan <nicholassull@gmail.com>
    Co-authored-by: Ryan Rendon<Rendon.S.Ryan@gmail.com>

---
+++ js/scripts copy.js
@@ -0,0 +1,5 @@
+const binaryNumber = "1100";
+
+console.log(parseInt(binaryNumber,2)); // base 2
+
+//output--> 12
---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+const binaryNumber = "1100";
+
+console.log(parseInt(binaryNumber,2));
+
+//output--> 12
+
+
+const hex = "c";
+
+console.log(parseInt(hex,16));  //hex base-16
+
+//output --> 12
+
